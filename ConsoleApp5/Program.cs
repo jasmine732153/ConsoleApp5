9fc513d238617c4f8c8f50ff25aec872804f77e5
visualstudio <visualstudio@122-PC33>
2019-04-17 08:46:05 -0400

    fourth commit

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace ConsoleApp5
 {   
@@ -11,27 +12,35 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
             new GenerateList().Run(57);
+            
         }
-         public static FindTwoNumbers()
-         {
-            return;
-         }
+         
 
         class GenerateList
         {
             const int LEN = 100;
             ArrayList ListQ;
+            int addTwoNumbers = 0;
+            public void FindTwoNumbers()
+            {
+                return;
+            }
+
 
             public void Run(int AddUpToNumber)
             {
                 Random r = new Random();
                 ListQ = new ArrayList();
+  
                 for (int i = 0; i < LEN; i++)
                 {
                     ListQ.Add(r.Next(100));
+                    
                 }
-               
-           }
+
+                Console.WriteLine("there are no two numbers that add up to 57");
+            }
+            
         }
     }
 }
