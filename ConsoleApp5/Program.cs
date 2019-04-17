42511fa16d82d2f821c6568e4c5735b7d50a9854
visualstudio <visualstudio@122-PC33>
2019-04-17 08:37:52 -0400

    third commit

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -5,21 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp5
-{
+{   
     class Program
     {
         static void Main(string[] args)
         {
             new GenerateList().Run(57);
         }
+         public static FindTwoNumbers()
+         {
+            return;
+         }
+
         class GenerateList
         {
             const int LEN = 100;
             ArrayList ListQ;
+
             public void Run(int AddUpToNumber)
             {
                 Random r = new Random();
-            }
+                ListQ = new ArrayList();
+                for (int i = 0; i < LEN; i++)
+                {
+                    ListQ.Add(r.Next(100));
+                }
+               
            }
         }
     }
